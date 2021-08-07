320e8abcf7c64e650cf3b376a9962c72dbaaaa2e
Christian Kimou <christian.85@live.fr>
2021-08-07 15:11:55 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<script>
+import AppHeader from "./components/AppHeader";
+
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style lang='scss'>
+@import "./css/global";
+@import "./css/typography";
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="w-full block p-4 border-b bg-blue-300 border-gray-700">
+    <h2 class="text-xl text-gray-800">Profile card Generator</h2>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang='scss' scoped>
+</style>
